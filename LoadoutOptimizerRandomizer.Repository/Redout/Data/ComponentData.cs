cca86b4d43e8a3456fe5d11619d9bdd5a89239f7
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-07-22 15:41:28 +0200

    adds propulsors

--- LoadoutOptimizerRandomizer.Repository/Redout/Data/ComponentData.cs
+++ LoadoutOptimizerRandomizer.Repository/Redout/Data/ComponentData.cs
@@ -169,6 +169,174 @@ public static class ComponentData
                     { Stat.Stability, 0 }
                 })
         },
+        {
+            "8-TA", new ShipComponent(ComponentType.Propulsor, "8-TA", "8-TA", ComponentClass.Green, 24,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 2 },
+                    { Stat.Thrust, 0 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "IDV1", new ShipComponent(ComponentType.Propulsor, "Draper v1", "IDV1", ComponentClass.Green, 48,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 4 },
+                    { Stat.Thrust, 0 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "F460", new ShipComponent(ComponentType.Propulsor, "F460", "F460", ComponentClass.Green, 66,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 3 },
+                    { Stat.Thrust, 3 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "8-TE", new ShipComponent(ComponentType.Propulsor, "8-TE", "8-TE", ComponentClass.Yellow, 102,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 6 },
+                    { Stat.Thrust, 3 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "IBV1", new ShipComponent(ComponentType.Propulsor, "Breda v1", "IBV1", ComponentClass.Yellow, 106,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 8 },
+                    { Stat.Thrust, 1 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "IDV2", new ShipComponent(ComponentType.Propulsor, "Draper v2", "IDV2", ComponentClass.Yellow, 144,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 12 },
+                    { Stat.Thrust, 0 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "F570", new ShipComponent(ComponentType.Propulsor, "F570", "F570", ComponentClass.Yellow, 148,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 9 },
+                    { Stat.Thrust, 4 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "8-TI", new ShipComponent(ComponentType.Propulsor, "8-TI", "8-TI", ComponentClass.Pink, 228,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 14 },
+                    { Stat.Thrust, 6 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "IBV2", new ShipComponent(ComponentType.Propulsor, "Breda v2", "IBV2", ComponentClass.Pink, 226,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 18 },
+                    { Stat.Thrust, 1 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "F690", new ShipComponent(ComponentType.Propulsor, "F690", "F690", ComponentClass.Pink, 224,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 12 },
+                    { Stat.Thrust, 8 },
+                    { Stat.Durability, 0 },
+                    { Stat.Strafe, 0 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "PSSU", new ShipComponent(ComponentType.Propulsor, "Signature Sulha", "PSSU", ComponentClass.Purple, 237,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 20 },
+                    { Stat.Thrust, 5 },
+                    { Stat.Durability, -6 },
+                    { Stat.Strafe, -1 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "PSBU", new ShipComponent(ComponentType.Propulsor, "Signature Mantis", "PSBU", ComponentClass.Purple, 237,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 14 },
+                    { Stat.Thrust, 10 },
+                    { Stat.Durability, -1 },
+                    { Stat.Strafe, -7 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "PSLN", new ShipComponent(ComponentType.Propulsor, "Signature Lunare", "PSLN", ComponentClass.Purple, 238,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 22 },
+                    { Stat.Thrust, 8 },
+                    { Stat.Durability, -5 },
+                    { Stat.Strafe, -6 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
+        {
+            "PSCO", new ShipComponent(ComponentType.Propulsor, "Signature Conqueror", "PSCO", ComponentClass.Purple, 237,
+                new Dictionary<Stat, int>
+                {
+                    { Stat.Speed, 22 },
+                    { Stat.Thrust, 2 },
+                    { Stat.Durability, -4 },
+                    { Stat.Strafe, -3 },
+                    { Stat.Steering, 0 },
+                    { Stat.Stability, 0 }
+                })
+        },
         
         // Stabilizers
         {
